4f672b3e4f5f0f6d2c1eedf22d12029f0b855475
Gaurav Sharma <gauraxvx0410@gmail.com>
2024-11-08 03:49:26 +0530

    function

--- Frontend-Assignment/script/script.js
+++ Frontend-Assignment/script/script.js
@@ -1,30 +1,39 @@
 
+
 const data = [
     {
         img: "./images/speaker-1.png",
         name: "John Doe",
         position: "Chief Marketing Officer",
-        company: 'Acme Corp'
+        company: 'Acme Corp',
+        bio: 'Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic theme areas.'
     },
     {
         img: "./images/speaker 2.png",
         name: "John Doe",
         position: "Chief Engagement Officer",
-        company: 'Acquia'
+        company: 'Acquia',
+        bio: 'Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic theme areas.'
+
     },
     {
         img: "./images/speaker 3.png",
         name: "John Doe",
         position: "Chief Technical Developer",
-        company: 'Pantheon'
+        company: 'Pantheon',
+        bio: 'Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic theme areas.'
+
     },
     {
         img: "./images/speaker 4.png",
         name: "John Doe",
         position: "Chief Marketing Officer",
-        company: 'Specbee'
+        company: 'Specbee',
+        bio: 'Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic theme areas.'
+
     }
 ]
+
 
 let cards = document.querySelector(".cards");
 
@@ -48,9 +57,94 @@ data.forEach((e)=>{
 
     card.append( img , name , position , company );
 
+    card.addEventListener('click' , ()=>{
+        localStorage.setItem('speaker', JSON.stringify(e));
+        display();
+    })
+
     cards.append(card);
 
-
-
-    
 })
+
+
+
+function display(){
+
+
+    let popup = document.querySelector('.popup');
+    popup.innerHTML = '';
+
+    let e = JSON.parse(localStorage.getItem('speaker'));
+
+    if(e){
+        let card = document.createElement("header");
+        card.classList.add('card-popup');
+
+        let img = document.createElement('img');
+        img.classList.add("img2");
+        img.src = e.img;
+
+        let profile = document.createElement("div");
+        profile.classList.add("profile-info");
+
+        let name = document.createElement("h2");
+        name.classList.add('name')
+        name.textContent = e.name;
+
+        let position = document.createElement('p');
+        position.classList.add("pos");
+        position.textContent = e.position;
+
+        let company = document.createElement("p");
+        company.classList.add("comp");
+        company.textContent = e.company;
+
+        let nav = document.createElement("nav");
+    nav.classList.add("social");
+
+    let vector = document.createElement('img');
+    vector.src = './images/vector.png'
+
+     let linkedin = document.createElement('img');
+    linkedin.src = './images/linkedin.png'
+
+     let twitter = document.createElement('img');
+    twitter.src = './images/X.png'
+
+    nav.append(vector , linkedin, twitter);
+
+    let cancel = document.createElement('button');
+    cancel.classList.add('close-btn')
+        cancel.innerHTML = `&times;`;
+
+        cancel.addEventListener("click" , ()=>{
+           localStorage.removeItem('speaker')   
+           popup.style.display = 'none';
+        })
+
+    profile.append(name , position , company , nav , cancel);
+
+   
+
+    card.append(img , profile );
+
+    let description = document.createElement("section");
+    description.classList.add('description');
+
+    let content = document.createElement('p');
+    content.textContent = e.bio;
+
+    description.append(content);
+
+    popup.append(card , description);
+
+
+
+    }
+
+}
+
+
+
+
+
